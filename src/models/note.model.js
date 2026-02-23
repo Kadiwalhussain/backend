145899bf3ca9bb40df638b2594b26bc77e6c6df3
Hussain <hussainkadiwal100@gmail.com>
2026-02-23 06:50:27 +0530

    chore: remove node_modules tracking and fix note model export

--- src/models/note.model.js
+++ src/models/note.model.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose');   
+const mongoose = require('mongoose');
 
-
-const noteSchema = new mongoose.Schema( {
-
-
+const noteSchema = new mongoose.Schema({
     title: String,
     description: String,
-    
+});
 
-})
+const noteModel = mongoose.model("note", noteSchema);
 
+module.exports = noteModel;
 
-const noteModel = mongoose.model("note", noteSchema)
+module.exports = noteModel
